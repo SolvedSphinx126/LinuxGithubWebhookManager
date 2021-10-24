f6029e3b10e1b4383ddf7841f88ea4c2d2b5213e
Ben O'Bryan <50787535+SolvedSphinx126@users.noreply.github.com>
2021-10-24 15:35:41 -0500

    Add support for start and stop scripts

--- reDeploy.sh
+++ reDeploy.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 if grep -Fxq "$1" repos.list
 then
+	if test -f ./stop.sh
+	then
+		./stop.sh
+		echo stopped process
+	fi
 	cd ../"$1"
 	git pull
         echo pulled repo "$1"
+	if test -f ./start.sh
+	then
+		./start.sh
+		echo started process
+	fi
 else
 	cd ..
 	mkdir "$1"
